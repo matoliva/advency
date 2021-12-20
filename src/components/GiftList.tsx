416a6a5ee3c0e8a256cc3680d050524a84d83578
Matias Oliva <olivamati@gmail.com>
2021-12-20 14:50:47 -0300

    day-8

--- src/components/GiftList.tsx
+++ src/components/GiftList.tsx
@@ -1,12 +1,11 @@
 import './gift-list.css'
-import {gifts} from '../data'
 
 export const GiftList = (props: any) => {
   return (
     <ul className="gift-list">
       {props.gifts.map((gift: any) => (
         <li key={gift.id} className="list-item">
-          {gift.name}
+          {`${gift.name} (${gift.quantity})`}
           <button
             className="btn btn-danger"
             onClick={() => props.handleDelete(gift.id)}
--- src/data.ts
+++ src/data.ts
@@ -4,21 +4,24 @@ export interface Gift {
   id: string
   name: string
   imgUrl?: string
-  amount?: number
+  quantity?: number
 }
 
 export const gifts: Gift[] = [
   {
     id: uuidv4(),
     name: 'Baloon',
+    quantity: 1,
   },
   {
     id: uuidv4(),
     name: 'Socks',
+    quantity: 1,
   },
   {
     id: uuidv4(),
     name: 'Battleship',
+    quantity: 1,
   },
 ]
 
